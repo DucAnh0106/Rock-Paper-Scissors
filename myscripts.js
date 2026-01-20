958ff79b41e16d8d756d21130fe2ba89ff64e22f
DucAnh0106 <233656241+DucAnh0106@users.noreply.github.com>
2026-01-20 15:29:15 +0800

    add pseudocode for the project

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+//Get the user's input and the computer's choice
+//intialize 2 variables to store user and computer's score and set their values to 0
+
+//Logic for one round
+//Who wins get the score incremented by 1
+
+//Logic for 5 rounds
+//While user's score or computer's score < 5, repeat the logic for one round
+//After the loop, check user's score if < 5, prints computer win and else, user wins
